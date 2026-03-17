d14e943630c8e8c12aae1f7e9f461639f0dd9ba8
rz <ruizhilim03@gmail.com>
2026-03-17 20:31:24 +0800

    move product to other js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,36 +1,10 @@
-// first: save the data
-const products = [{
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-        stars: 4.5,
-        counts: 56
-    },
-    priceCents: 799
-}];
+// first: save the data go to products.js
 
 // second: Generate HTML
-
 let productsHTML = ``;
 
 products.forEach((product) => {
-    productsHTML += `        
+  productsHTML += `        
     <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -45,7 +19,7 @@ products.forEach((product) => {
             <img class="product-rating-stars"
               src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.counts}
+              ${product.rating.count}
             </div>
           </div>
 
