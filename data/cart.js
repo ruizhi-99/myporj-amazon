2b83853286f81b4beca652bb8da30f9f53773740
rz <ruizhilim03@gmail.com>
2026-03-20 13:00:22 +0800

    save to local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
-export let cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
-}, {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+    cart = [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2
+    }, {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1
+    }];
+}
+
+function saveTostorage() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
 
 export function addTocart(productId) {
     let matchingItem;
@@ -23,14 +32,18 @@ export function addTocart(productId) {
             quantity: 1
         });
     }
+    saveTostorage();
 }
 
-export function removeCart(productId){
-    const newCart= [];
-    cart.forEach((cartItem)=>{
-        if(cartItem.productId !== productId){
+
+
+export function removeCart(productId) {
+    const newCart = [];
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
             newCart.push(cartItem);
         }
     });
     cart = newCart;
+    saveTostorage();
 }
