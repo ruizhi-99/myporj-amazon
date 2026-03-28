d5e959de52c33169ef47110327ab3ba5236793f2
rz <ruizhilim03@gmail.com>
2026-03-28 18:18:26 +0800

    added class to product

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import formatCurrency from "../scripts/money.js";
+
 export function getProduct(productId){
   let matchingProduct;
 
@@ -9,6 +11,30 @@ export function getProduct(productId){
     return matchingProduct;
 }
 
+class Product{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+}
+ 
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -668,4 +694,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails)=>{
+  return new Product(productDetails);
+});
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import { renderOrderSummary } from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutheader.js";
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
+
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
