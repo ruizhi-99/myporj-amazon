829252705893abcd5ec7378d42e47823c94d9caf
rz <ruizhilim03@gmail.com>
2026-06-16 21:12:55 +0800

    Update cart-oop.js

--- data/cart-oop.js
+++ data/cart-oop.js
@@ -11,22 +11,6 @@ function Cart(localStorageKey) {
             } catch {
                 this.cartItems = [];
             }
-
-            if (this.cartItems.length === 0) {
-                this.cartItems = [{
-                    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-                    quantity: 2,
-                    deliveryOptionId: '1'
-                }, {
-                    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-                    quantity: 1,
-                    deliveryOptionId: '2'
-                }];
-            }
-        },
-
-        saveToStorage() {
-            localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
         },
 
         addToCart(productId, quantity = 1) {
